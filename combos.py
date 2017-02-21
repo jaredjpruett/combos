d1286108b4864c71e2ca8ed0458076904c2db430
Jared Pruett <jaredjpruett@gmail.com>
2017-02-21 10:59:43 -0600

    changes

--- combos.py
+++ combos.py
@@ -1,23 +1,50 @@
+import os
+import sys
 import json
+import ntpath
+import getpass
 import itertools
+import subprocess
+
+################################################################################
+# Usage                                                                        #
+################################################################################
+
+if len(sys.argv) != 3:
+	sys.stderr.write("Usage: python %s <archive> <separator>\n" % sys.argv[0])
+	sys.stderr.write("   Ex: python %s test.7z '|'\n")
+	sys.exit(1)
 
 ################################################################################
 # Global definitions                                                           #
 ################################################################################
 
-input_file = "words"
-output_file = "combos"
-
-separator = " "
+archive = sys.argv[1]
+separator = sys.argv[2]
 
 ################################################################################
 # Function definitions                                                         #
 ################################################################################
 
-def read_words():
-	return [ word.strip('\n') for word in open(input_file).readlines() ]
+def prompt_words():
+	words = []
 
-def enumerate_combos(words):
+	word = True
+	while word:
+		word = getpass.getpass("Enter word (empty line to stop): ")
+		if word:
+			if word == getpass.getpass("Confirm word: "):
+				words.append(word)
+			else:
+				sys.stderr.write("Words did not match.\n")
+			print
+	
+	print "\nRead %d words.\n" % len(words)
+	
+	return words
+
+def enumerate_combos():
+	words = prompt_words()
 	combos = []
 
 	for x in range(0, len(words)):
@@ -27,14 +54,22 @@ def enumerate_combos(words):
 	
 	return combos
 
-def write_combos(combos):
-	fp = open(output_file, "w")
+def attempt_combos(combos):
+	FNULL = open(os.devnull, 'w')
 
 	for combo in combos:
-		fp.write("%s\n" % combo)
+		command = [ "7z", "t", "-p%s" % combo, archive ]
+
+		rc = subprocess.call(command, stdout=FNULL)
+
+		if not rc:
+			print "Success with combo '%s'." % combo
+			return
+
+	print "No luck."
 
 ################################################################################
 # Main                                                                         #
 ################################################################################
 
-write_combos(enumerate_combos(read_words()))
+attempt_combos(enumerate_combos())
