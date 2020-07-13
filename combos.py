4e44097d5839ffd1f39f4292786cf1c1284e7039
Jared Pruett <jaredjpruett@gmail.com>
2020-07-13 05:24:22 -0500

    periodic commit

--- combos.py
+++ combos.py
@@ -6,13 +6,18 @@ import getpass
 import itertools
 import subprocess
 
+# TODO:
+# Count possible permutations
+# Track tries
+# Hide error text
+
 ################################################################################
 # Usage                                                                        #
 ################################################################################
 
-if len(sys.argv) != 3:
-	sys.stderr.write("Usage: python %s <archive> <separator>\n" % sys.argv[0])
-	sys.stderr.write("   Ex: python %s test.7z '|'\n")
+if len(sys.argv) != 2 and len(sys.argv) != 3:
+	sys.stderr.write("Usage: python %s <archive> [separator]\n" % sys.argv[0])
+	sys.stderr.write("   Ex: python %s test.7z '|'\n" % sys.argv[0])
 	sys.exit(1)
 
 ################################################################################
@@ -20,7 +25,8 @@ if len(sys.argv) != 3:
 ################################################################################
 
 archive = sys.argv[1]
-separator = sys.argv[2]
+#separator = sys.argv[2]
+separator = " "
 
 ################################################################################
 # Function definitions                                                         #
@@ -33,43 +39,50 @@ def prompt_words():
 	while word:
 		word = getpass.getpass("Enter word (empty line to stop): ")
 		if word:
-			if word == getpass.getpass("Confirm word: "):
-				words.append(word)
-			else:
-				sys.stderr.write("Words did not match.\n")
-			print
+			words.append(word)
 	
-	print "\nRead %d words.\n" % len(words)
+	print "Read %d words.\n" % len(words)
 	
 	return words
 
 def enumerate_combos():
-	words = prompt_words()
-	combos = []
+	combos = [ ]
 
+	words = prompt_words()
 	for x in range(0, len(words)):
 		for combo in itertools.combinations(words, x + 1):
 			for perm in itertools.permutations(list(combo)):
 				combos.append(separator.join(perm))
+
+	print "Enumerated %d combinations.\n" % len(combos)
 	
 	return combos
 
 def attempt_combos(combos):
+	attempt = 0
 	FNULL = open(os.devnull, 'w')
-
 	for combo in combos:
+		attempt += 1
+		if attempt % 25 == 0:
+			print "Attempt %d" % attempt
 		command = [ "7z", "t", "-p%s" % combo, archive ]
-
-		rc = subprocess.call(command, stdout=FNULL)
-
+		rc = subprocess.call(command, stdout=FNULL, stderr=FNULL)
 		if not rc:
 			print "Success with combo '%s'." % combo
 			return
 
 	print "No luck. Tried %d combinations." % len(combos)
 
+def dump_combos(combos):
+    print "Dumping combos...\n"
+    fout = open("combos.txt", "w")
+    for combo in combos:
+        fout.write(combo + "\n")
+
 ################################################################################
 # Main                                                                         #
 ################################################################################
 
-attempt_combos(enumerate_combos())
+combos = enumerate_combos()
+dump_combos(combos)
+attempt_combos(combos)
